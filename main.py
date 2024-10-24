74cede29c72d633e4eb4b1dbd159540db7078197
Hasnain Iqbal <bsse1106@iit.du.ac.bd>
2024-10-24 15:29:20 +0600

    just expected setting left

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 from astextractor import ASTExtractor
 from find_shared_states import find
+from repos import projects
 
 ast_extractor = ASTExtractor("ASTExtractor-0.5.jar", "ASTExtractor.properties")
 
@@ -29,30 +30,7 @@ def gen_ast(project_dir, output_file):
     return ast
 
 if __name__ == "__main__":
-    projects = [
-        {
-            "repo": "io/repos/Activiti-b11f757a48600e53aaf3fcb7a3ba1ece6c463cb4/activiti-spring-boot-starter/src",
-            "identifier": "Activiti-b11f757a48600e53aaf3fcb7a3ba1ece6c463cb4-activiti-spring-boot-starter",
-            "original_order":"io/original_orders/Activiti_Activiti-activiti-spring-boot-starter-b11f757-original_order"
-        },
-        {
-            "repo": "io/repos/fastjson-5c6d6fd471ea1fab59f0df2dd31e0b936806780d/src",
-            "identifier": "fastjson-5c6d6fd471ea1fab59f0df2dd31e0b936806780d",
-            "original_order":"io/original_orders/alibaba_fastjson-.-5c6d6fd-original_order"
-        },
-        {
-            "repo": "io/repos/hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea/hadoop-common-project/hadoop-auth/src",
-            "identifier": "hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea-hadoop-common-project-hadoop-auth",
-            "original_order":"io/original_orders/apache_hadoop-hadoop-common-project_hadoop-auth-cc2babc-original_order"
-        },
-        {
-            "repo": "io/repos/hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea/hadoop-hdfs-project/hadoop-hdfs-nfs/src",
-            "identifier": "hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea-hadoop-hdfs-project-hadoop-hdfs-nfs",
-            "original_order":"io/original_orders/apache_hadoop-hadoop-hdfs-project_hadoop-hdfs-nfs-cc2babc-original_order"
-        },
-        # Add more directories here
-        
-    ]
+    projects = projects
     
     for project in projects:
         gen_ast(project["repo"], project["identifier"])
---
+++ repos.py
@@ -0,0 +1,237 @@
+projects = [
+    {
+        "repo": "io/repos/Activiti-b11f757a48600e53aaf3fcb7a3ba1ece6c463cb4/activiti-spring-boot-starter/src",
+        "identifier": "Activiti-b11f757a48600e53aaf3fcb7a3ba1ece6c463cb4-activiti-spring-boot-starter",
+        "original_order":"io/original_orders/Activiti_Activiti-activiti-spring-boot-starter-b11f757-original_order"
+    },
+    {
+        "repo": "io/repos/fastjson-5c6d6fd471ea1fab59f0df2dd31e0b936806780d/src",
+        "identifier": "fastjson-5c6d6fd471ea1fab59f0df2dd31e0b936806780d",
+        "original_order":"io/original_orders/alibaba_fastjson-.-5c6d6fd-original_order"
+    },
+    {
+        "repo": "io/repos/hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea/hadoop-common-project/hadoop-auth/src",
+        "identifier": "hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea-hadoop-common-project-hadoop-auth",
+        "original_order":"io/original_orders/apache_hadoop-hadoop-common-project_hadoop-auth-cc2babc-original_order"
+    },
+    {
+        "repo": "io/repos/hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea/hadoop-hdfs-project/hadoop-hdfs-nfs/src",
+        "identifier": "hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea-hadoop-hdfs-project-hadoop-hdfs-nfs",
+        "original_order":"io/original_orders/apache_hadoop-hadoop-hdfs-project_hadoop-hdfs-nfs-cc2babc-original_order"
+    },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea-hadoop-mapreduce-project-hadoop-mapreduce-client-hadoop-mapreduce-client-app",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea-hadoop-mapreduce-project-hadoop-mapreduce-client-hadoop-mapreduce-client-core",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "hadoop-cc2babc1f75c93bf89a8f10da525f944c15d02ea-hadoop-mapreduce-project-hadoop-mapreduce-client-hadoop-mapreduce-client-hs",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "dubbo-737f7a7ea67832d7f17517326fb2491d0a086dd7-dubbo-cluster",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "dubbo-737f7a7ea67832d7f17517326fb2491d0a086dd7-dubbo-common",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "dubbo-737f7a7ea67832d7f17517326fb2491d0a086dd7-dubbo-config-dubbo-config-api",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "dubbo-737f7a7ea67832d7f17517326fb2491d0a086dd7-dubbo-filter-dubbo-filter-cache",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "dubbo-737f7a7ea67832d7f17517326fb2491d0a086dd7-dubbo-rpc-dubbo-rpc-api",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "dubbo-737f7a7ea67832d7f17517326fb2491d0a086dd7-dubbo-rpc-dubbo-rpc-dubbo",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "dubbo-737f7a7ea67832d7f17517326fb2491d0a086dd7-dubbo-serialization-dubbo-serialization-fst",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "Struts-13d9053050c9e4fb2ef049db6a37d3f6eebf48fa-plugins-portlet",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "c2mon-d80687b119c713dd177a58cf53a997d8cc5ca264-c2mon-server-c2mon-server-elasticsearch",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "cukes-b483e1a8f261b80a66291a42fc455256b0b5059c-cukes-http",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "Achilles-e3099bdce342910951c4862c78751fd81ed4552e-integration-test-2_1",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "Achilles-e3099bdce342910951c4862c78751fd81ed4552e-integration-test-2_2",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "Achilles-e3099bdce342910951c4862c78751fd81ed4552e-integration-test-3_10",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "Achilles-e3099bdce342910951c4862c78751fd81ed4552e-integration-test-3_7",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "dropwizard-07dfaed697427e208d65049f80a5d1949833b7cd-dropwizard-logging",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "elastic-job-lite-b022898ef1b8c984e17efb2a422ee45f6b13e46e-elastic-job-lite-core",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "hsac-fitnesse-fixtures-a64c18d9c4bac8271275c7b089d40be20f0604b5-",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "spring-data-ebean-dd11b97654982403b50dd1d5369cadad71fce410-",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "jhipster-registry-00db36611da5fc7aaf9d5372aa90f2465d80c0c4-",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "http-request-2d62a3e9da726942a93cf16b6e91c0187e6c0136-lib",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "marine-api-af0003847db9ba822f67d4f1dceb8de3fe63250a-",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "openpojo-9badbcc4593e797accfed5e51bec9f2b843f0f67-",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "spring-boot-daa3d457b71896a758995c264977bdd1414ee4d4-spring-boot-project-spring-boot-actuator-autoconfigure",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "spring-boot-daa3d457b71896a758995c264977bdd1414ee4d4-spring-boot-project-spring-boot",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "spring-boot-daa3d457b71896a758995c264977bdd1414ee4d4-spring-boot-project-spring-boot-test-autoconfigure",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "spring-boot-daa3d457b71896a758995c264977bdd1414ee4d4-spring-boot-project-spring-boot-test",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "spring-data-envers-5637994be37747e82b2d6d5b34555e2bee791fe6-",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "spring-ws-e8d89c9eb0929dda304174729c9c69fb29f448eb-spring-ws-core",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "spring-ws-e8d89c9eb0929dda304174729c9c69fb29f448eb-spring-ws-security",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "aismessages-7b0c4c708b6bb9a6da3d5737bcad1857ade8a931-",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "unix4j-367da7d262e682a08577cdf19ebbbdd8a46870fe-unix4j-core-unix4j-command",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "admiral-e4b02936cc7d4ff2714e7231db0c4373ba5d48a2-compute",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "admiral-e4b02936cc7d4ff2714e7231db0c4373ba5d48a2-request",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "wikidata-toolkit-20de6f7f12319f54eb962ff6e8357b3f5695d54d-wdtk-dumpfiles",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "wikidata-toolkit-20de6f7f12319f54eb962ff6e8357b3f5695d54d-wdtk-util",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "wildfly-b19048b72669fc0e96665b1b125dc1fda21f5993-naming",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "wildfly-b19048b72669fc0e96665b1b125dc1fda21f5993-security-subsystem",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "wro4j-185ab607f1d649ca38b4a772831ee754cd4649fb-wro4j-core",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "carbon-apimgt-a82213e40e7e6aa529341fdd1d1c3de776949e64-components-apimgt-org.wso2.carbon.apimgt.core",
+    #     "original_order": "order"
+    # },
+    # {
+    #     "repo": "repo",
+    #     "identifier": "riptide-8277e11fc069d8e24df0d233ef2577cc75659b75-riptide-spring-boot-starter",
+    #     "original_order": "order"
+    # },
+]
